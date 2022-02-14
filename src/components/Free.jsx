19e1b1a8cd1a718fd1a49cfc3acfdd183317d577
Padilha1 <padilha.matheus@hotmail.com>
2022-02-14 17:19:38 -0300

    Finished Free section

--- src/components/Free.jsx
+++ src/components/Free.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import super1 from "../assets/super1.png";
 import super2 from "../assets/super2.png";
 import release2 from "../assets/release2.png";
-import Rak from "../assets/Rak.png";
+import eth1 from "../assets/eth1.png";
+import Card from './Card';
 
 
 export default function Free() {
@@ -15,15 +16,29 @@ export default function Free() {
           </div>
           <div className="content">
             <div className="image">
-              <img src={Rak} alt="icon" />
+              <img src={eth1} alt="icon" />
             </div>
             <h2 className="title">Free NFT for early birds</h2>
             <p className="description">Sign up and get a free NFT</p>
           </div>
         </div>
         <div className="cards">
-          <div className="card1"></div>
-          <div className="card2"></div>
+          <div className="card1">
+            <Card 
+            image={super1}
+            series="Floop Series"
+            title="Purple Man"
+            price={2.99} tag={12893} time={1}
+            />
+          </div>
+          <div className="card2">
+          <Card 
+            image={super2}
+            series="Gloop Series"
+            title="Purple Man"
+            price={3.95} tag={15435} time={1}
+            />
+          </div>
         </div>
 
 
